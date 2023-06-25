6f20ac515a4b930e75d755452146443f8981dcaa
Andreas-Lych <caesar.a.1981@gmail.com>
2023-06-25 22:58:40 +0300

    create suser+runserver. all ok

--- luxury/settings.py
+++ luxury/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+#and SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(=2(3nrwpf%(5f5l@b44tel^f-j+21c(j6=+hfsr)raa2h0d1d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'luxury.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "luxury",
+        "USER": "luxury",
+        "PASSWORD": "luxury",
+        "HOST": "localhost",
+        "PORT": 5432,
+   }
 }
+
 
 
 # Password validation
