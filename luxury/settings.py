9270fc13dac11401ebdeffc451ce02a7b9947b87
Andreas-Lych <caesar.a.1981@gmail.com>
2023-06-29 00:24:58 +0300

    drop base and migration

--- luxury/settings.py
+++ luxury/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rent',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Custom user model
+
+AUTH_USER_MODEL = 'users.User'
+
+AUTHENTICATION_BACKENDS = (
+    'users.backends.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
