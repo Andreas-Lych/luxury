3e6787ec5e23350bf1db41d3506f8f392c245dc6
Andreas-Lych <caesar.a.1981@gmail.com>
2023-07-06 21:50:46 +0300

    add product model CHOICES: color, gearbox,fuel+migrations

--- rent/admin.py
+++ rent/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from rent.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "created_at", "color", "gearbox", "fuel",)
+    fields = ("title", "image", "price", "created_at", "color", "gearbox", "fuel",)
+    readonly_fields = ("created_at",)
+    search_fields = ("title", "description")
+
--- rent/models.py
+++ rent/models.py
@@ -2,11 +2,31 @@ from decimal import Decimal
 
 from django.db import models
 
+COLOR_CHOICES = (
+    ("RED", "Red"),
+    ("GREEN", "Green"),
+    ("BLUE", "Blue"),
+    ("WHITE", "White"),
+)
+
+GEARBOX_CHOICES = (
+    ("AT", "Automatic transmission"),
+    ("MT", "Manual transmission"),
+)
+
+FUEL_CHOICES = (
+    ("GAS", "Gas"),
+    ("DISEL", "Disel"),
+    ("ELECTRO", "Electro"),
+)
 
 class Product(models.Model):
     external_id = models.CharField(max_length=255, blank=True, null=True)
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to="products/", blank=True, null=True)
+    color = models.CharField(max_length=32, choices=COLOR_CHOICES, default="WHITE")
+    gearbox = models.CharField(max_length=32, choices=GEARBOX_CHOICES, default="AT")
+    fuel = models.CharField(max_length=32, choices=FUEL_CHOICES, default="GAS")
     price = models.DecimalField(default=Decimal("0"), decimal_places=5, max_digits=10)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(
