fedaf3efff1afe10aaba32005d05ac727b522bbc
Andreas-Lych <caesar.a.1981@gmail.com>
2023-07-08 00:35:32 +0300

    debag mode with profiles

--- profiles/forms.py
+++ profiles/forms.py
@@ -8,4 +8,7 @@ class ProfilesForm(forms.Form):
     password = forms.CharField(
         min_length=8, widget=forms.PasswordInput()
     )
+    passport = forms.CharField(max_length=255)
+    phone = forms.CharField(max_length=12)
     age = forms.IntegerField(min_value=18, required=False)
+    driving_experience = forms.IntegerField(min_value=3, required=False)
